f86706224b982b78fec168175017ed459eeba01f
Vishal Ponugoti <vponugot@purdue.edu>
2015-02-25 13:15:14 -0500

    add pe05.c

---
+++ hw/PE05/pe05.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "answer05.h"
+
+int main(int argc, char *argv[])
+{ 
+   printf("Welcome to ECE264, we are working on PE05.\n\n");
+
+   if (argc < 2) {
+      return EXIT_FAILURE;
+   }
+   int n = atoi(argv[1]);
+   if (n < 1) {
+      return EXIT_FAILURE;
+   }
+   if (ato1(argv[1] == 1) {
+      Find_maze_dimensions();
+   
+   return EXIT_SUCCESS;
+}
+
+void readFile(char * fileName)
+{
+   FILE * fptr = fopen(fileName, "r");
+   int val;
+   if (fptr == NULL) {
+      fprintf(stderr, "cannot read file %s\n", fileName);
+      return;
+   }
+   while (! feof(fptr)) {
+      fscanf(fptr, "%d\n", & val);
+      printf("%d\n", val);
+   }
+   fclose(fptr);
+}
